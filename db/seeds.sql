6f3f1e9fb3957272ef3a4b434d30c9bd36698312
Ryan Norrid <rnorrid@gmail.com>
2017-09-09 22:30:43 -0600

    added functionality to the app, database works.

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Patty Melt', false);
